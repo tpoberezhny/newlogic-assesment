7b12b2806eae07738fd98b9f3ded4fe5f9c0f131
Tim <poberezhny003@gmail.com>
2024-11-09 19:42:13 +0100

    reuseble styles

---
+++ src/styles.js
@@ -0,0 +1,93 @@
+const styles = {
+  filterWhite: {
+    filter: "brightness(0) invert(1)",
+  },
+  textXs: {
+    fontSize: "0.8rem",
+    fontWeight: 500,
+    lineHeight: "1.25rem",
+  },
+  textSm: {
+    fontSize: "0.875rem",
+    fontWeight: 500,
+    lineHeight: "1.25rem",
+  },
+  textBase: {
+    fontSize: "1rem",
+    fontWeight: 400,
+    lineHeight: "1.5rem",
+  },
+  textLg: {
+    fontSize: "1.125rem",
+    fontWeight: 600,
+    lineHeight: "1.75rem",
+  },
+  textXl: {
+    fontSize: "1.25rem",
+    fontWeight: 400,
+    lineHeight: "1.875rem",
+  },
+  text2Xl: {
+    fontSize: "1.25rem",
+    fontWeight: 700,
+    lineHeight: "2.25rem",
+  },
+  text3Xl: {
+    fontSize: "1.5rem",
+    fontWeight: 500,
+    lineHeight: "2.25rem",
+  },
+  text4Xl: {
+    fontSize: "2rem",
+    fontWeight: 400,
+    lineHeight: "3rem",
+  },
+  text5Xl: {
+    fontSize: "2.25rem",
+    fontWeight: 400,
+    lineHeight: "3rem",
+  },
+  text6Xl: {
+    fontSize: "3rem",
+    fontWeight: 400,
+    lineHeight: "4rem",
+  },
+  text7Xl: {
+    fontSize: "4rem",
+    fontWeight: 400,
+    lineHeight: "1",
+  },
+  text8Xl: {
+    fontSize: "5rem",
+    fontWeight: 400,
+    lineHeight: "1",
+  },
+  uiBtnDefault: {
+    fontSize: "1rem",
+    fontWeight: 600,
+    lineHeight: "1,5rem"
+  },
+  heading: {
+    fontSize: "2rem",
+    fontWeight: 600,
+    lineHeight: "2,5rem"
+  },
+  smTitle: {
+    fontSize: "0.9rem",
+    fontWeight: 600,
+    lineHeight: "1.375rem"
+  },
+  paginationEl: {
+    fontSize: "1rem",
+    fontWeight: 400,
+    lineHeight: "1.5rem",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "2rem",
+    height: "2rem",
+    backgroundColor: "#ffffff",
+    borderRadius: "0.25rem"
+  }
+};
+export default styles;
