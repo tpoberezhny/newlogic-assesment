fdc2ac028dbb64c768de631b66bc518a79cd93bf
Tim <poberezhny003@gmail.com>
2024-11-11 19:10:42 +0100

    Adding starting path + settings route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import OrderManagement from "./pages/OrderManagment";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
+import OrderManagement from "./pages/OrderManagment";
 import Invoices from "./pages/Invoices";
+import AccountSettings from "./pages/AccountSettings";
 
 function App() {
   return (
     <Router>
       <div className="flex-1 flex flex-col">
         <Routes>
+          <Route
+            path="/"
+            element={<Navigate to="/order-management" replace />}
+          />
           <Route path="/order-management" element={<OrderManagement />} />
           <Route path="/faktury" element={<Invoices />} />
+          <Route path="/settings" element={<AccountSettings />} />
         </Routes>
       </div>
     </Router>
